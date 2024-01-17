df0bd6b184063bdaa80b8d7af0757a5d704f683a
juandiego2012 <140218956+juandiego2012@users.noreply.github.com>
2024-01-17 14:35:16 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,17 @@
+// valor aleatorio generado
+var y = math.floor(math.random() * 1 + 10);
+  
+// contar el número de intentos
+if (x == y)
+{
+    alert("¡¡¡FELICIDADES!!!" +playername+ " LO ADIVINASTE BIEN "
+    + guess + " INTENTO ");
+    
+    guess= 1;
+}
+// crear para el intento correcto
+
+  
+// función para el número adivinado por el usuario 
+var x = document.getElementById("guessField").value;
+
---
+++ login.js
@@ -0,0 +1,9 @@
+// crea la función de inicio de sesión.
+
+function login() {
+
+    player_name = document.getElementById("playername").value;
+    localStorage.setItem("player_name", player_name);
+
+    window.location = "gamepage.html";
+}
